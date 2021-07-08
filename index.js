060371742aac2ea405772238da09f8f153522ec9
RetroGeek46 <divyangpradeep@gmail.com>
2021-07-09 02:07:36 +0530

    Add socket.io

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const electron = require('electron');
 const path = require("path");
-
-const { app, BrowserWindow, Tray, Menu, Notification, dialog, clipboard } = electron;
+const server = require("./server.js");
+const { app, BrowserWindow, Tray, Menu, dialog, clipboard, globalShortcut } =
+    electron;
 
 let mainWindow;
 
-function createMainWindow() {
+createMainWindow = () => {
     let win = new BrowserWindow({
         width: 900,
         height: 493,
@@ -19,72 +20,66 @@ function createMainWindow() {
         center: true,
         thickFrame: true,
     });
-    // const url = "http://localhost:3002";
-
-    // win.loadURL(url).then(() => console.log("URL loaded."));
-
     let tray = null;
     win.on('minimize', function (event) {
         event.preventDefault();
         win.hide();
         tray = createTray();
     });
-
     win.on('restore', function (event) {
         win.show();
         tray.destroy();
     });
-
     return win;
 }
 
-function createTray() {
+sendSnip = () => {
+    let clipboardImage = clipboard.readImage();
+    if (clipboardImage.isEmpty()) {
+        const errorOptions = {
+            buttons: ["OK"],
+            title: "Invalid Data",
+            message: "The data in clipboard is not a valid image",
+        };
+        dialog.showMessageBox(null, errorOptions);
+    } else {
+        // const successOptions = {
+        //     buttons: ["OK"],
+        //     title: "Data Sent",
+        //     message: "Data sent successfully",
+        // };
+        // dialog.showMessageBox(null, successOptions);
+        server.emitMessage('snipShare', clipboardImage.toDataURL());
+    }
+}
+
+createTray = () => {
     let appIcon = new Tray(path.join(__dirname, "cloud_fun.ico"));
     const contextMenu = Menu.buildFromTemplate([
-        {
-            label: 'Send Snippet', click: function () {
-                let clipboardImage = clipboard.readImage()
-                // console.log(clipboardImage.isEmpty());
-                // console.log(clipboardImage.getSize());
-                if (clipboardImage.isEmpty()) {
-                    // new Notification({ title: "Invalid Data", body: "The data in clipboard is not a valid image" }).show();
-                    const errorOptions = {
-                        buttons: ['OK'],
-                        title: 'Invalid Data',
-                        message: 'The data in clipboard is not a valid image'
-                    };
-                    dialog.showMessageBox(null, errorOptions);
-                    console.log('no image');
-                } else {
-                    const successOptions = {
-                        buttons: ['OK'],
-                        title: 'Data Sent',
-                        message: 'Data sent successfully'
-                    };
-                    dialog.showMessageBox(null, successOptions);
-                    console.log('yes image');
-                }
-            }
-        },
-        {
-            label: 'Exit', click: function () {
-                app.isQuiting = true;
+        { label: 'Send Snippet', click: function () {sendSnip()} },
+        { label: 'Exit', click: function () {
+                app.isQuitting = true;
                 app.quit();
             }
         }
     ]);
-
-    appIcon.on('double-click', function (event) {
+    appIcon.on('double-click', (event) => {
         mainWindow.show();
     });
-
     appIcon.setToolTip('SnipShare');
     appIcon.setContextMenu(contextMenu);
     return appIcon;
 }
 
 app.on('ready', () => {
-    console.log('app is ready');
+    console.log('App Running');
+    const globalShortcutRegister = globalShortcut.register("Ctrl+Alt+9", () => {
+        sendSnip();
+    });
     mainWindow = createMainWindow();
     mainWindow.minimize();
 });
+
+app.on('will-quit', () => {
+    globalShortcut.unregisterAll();
+})
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3444",
+        methods: ["GET", "POST"],
+    },
+});
+
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/index.html");
+// });
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+
+    socket.on("snipShare", (msg) => {
+        console.log("message: " + msg);
+    });
+});
+
+exports.emitMessage = (tag, message) => {
+    io.emit(tag, message)
+}
+
+server.listen(3456, () => {
+    console.log("listening on *:3456");
+});
